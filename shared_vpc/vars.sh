3d06acb7e4894adf5e3242bc987b8981edcc3d94
Ameer Abbas <ameer00@gmail.com>
2018-06-25 22:22:58 -0700

    Update vars.sh

--- shared_vpc/vars.sh
+++ shared_vpc/vars.sh
@@ -1,3 +1,8 @@
+// Tutorial: https://cloud.google.com/community/tutorials/managing-gcp-projects-with-terraform
+// Change org_id, billing_id to match your org/bill
+// TF_ADMIN should be the terraform host project, which can create other projects
+// TF_CREDS is the location of the service account JSON key file
+ 
 export TF_VAR_org_id=689680127547
 export TF_VAR_billing_account=00CF06-C4A4BE-92FDD8
 export TF_ADMIN=theplatform-terraform-admin
