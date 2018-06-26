64abb5d28d41c52fe1b6f6d3b4445d006c60b3a4
Ameer Abbas <ameer00@gmail.com>
2018-06-25 22:20:51 -0700

    Create vars.sh

---
+++ shared_vpc/vars.sh
@@ -0,0 +1,4 @@
+export TF_VAR_org_id=689680127547
+export TF_VAR_billing_account=00CF06-C4A4BE-92FDD8
+export TF_ADMIN=theplatform-terraform-admin
+export TF_CREDS=~/.config/gcloud/terraform-admin.json
