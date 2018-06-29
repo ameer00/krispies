5f60cfacf9f34a483663ced471031d86dd5de088
Ameer Abbas <ameer00@gmail.com>
2018-06-28 22:41:54 -0700

    Create main.tf

---
+++ mods/provider/main.tf
@@ -0,0 +1,10 @@
+variable "credentials" { default = "~/.config/gcloud/terraform-admin.json" }
+variable "project" { default = "krispies-tf-admin" }
+variable "region" { default = "us-central1" }
+
+provider "google" {
+ version     = "~> 1.15"
+ credentials = "${file("${var.credentials}")}"
+ project     = "${var.project}"
+ region      = "${var.region}"
+}
