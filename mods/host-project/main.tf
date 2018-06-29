2339188693368668ca517830ce7833b220d374b3
Ameer Abbas <ameer00@gmail.com>
2018-06-28 22:43:20 -0700

    Create main.tf

---
+++ mods/host-project/main.tf
@@ -0,0 +1,11 @@
+variable "host_project" { default = "krispies1000-host-project" }
+variable "billing_account" { default = "00CF06-C4A4BE-92FDD8" }
+variable "org_id" { default = "990456989270" }
+
+// Create Host project
+resource "google_project" "host" {
+ name            = "${var.host_project}"
+ project_id      = "${var.host_project}"
+ billing_account = "${var.billing_account}"
+ org_id          = "${var.org_id}"
+}
