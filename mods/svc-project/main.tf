f4977ad1f0f590e01ca31d0c45acf29b620a51cd
Ameer Abbas <ameer00@gmail.com>
2018-06-28 23:29:54 -0700

    Update main.tf

--- mods/svc-project/main.tf
+++ mods/svc-project/main.tf
@@ -5,8 +5,8 @@ variable "create_subs" { default = "false" }
 
 resource "google_project" "project" {
  count		       = "${var.count}"
- name            = "krispies1000-svc-${count.index+1}"
- project_id      = "krispies1000-svc-${count.index+1}"
+ name            = "krispies1100-svc-${count.index+1}"
+ project_id      = "krispies1100-svc-${count.index+1}"
  billing_account = "${var.billing_account}"
  org_id          = "${var.org_id}"
  auto_create_network = "${var.create_subs}"
