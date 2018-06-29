ce511f420205eefbddb92468ef50bf0fd816fe47
Ameer Abbas <ameer00@gmail.com>
2018-06-28 23:44:01 -0700

    Update main.tf

--- mods/svc-project/main.tf
+++ mods/svc-project/main.tf
@@ -2,6 +2,7 @@ variable "count" { default = "2" }
 variable "billing_account" { default = "00CF06-C4A4BE-92FDD8" }
 variable "org_id" { default = "990456989270" }
 variable "create_subs" { default = "false" }
+variable "host_project" {}
 
 resource "google_project" "project" {
  count		       = "${var.count}"
@@ -22,4 +23,17 @@ resource "google_project_service" "project-container" {
  count   = "${var.count}"
  project = "${element(google_project.project.*.project_id, count.index)}"
  service = "container.googleapis.com"
+}
+
+// Enable shared VPC on service projects - explicitly depend on the host
+// project enabling it, because enabling shared VPC will fail if the host project
+//# is not yet hosting.
+resource "google_compute_shared_vpc_service_project" "service_projects" {
+  count		         = "${var.count}"
+  host_project    = "${var.host_project}"
+  service_project = "${element(google_project.project.*.project_id, count.index)}"
+
+//  depends_on = ["google_compute_shared_vpc_host_project.host",
+//    "google_project_service.project",
+//  ]
 }
