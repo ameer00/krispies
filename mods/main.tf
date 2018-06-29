725b79980a0d3dc51277c9622b7e75890338c645
Ameer Abbas <ameer00@gmail.com>
2018-06-28 23:03:16 -0700

    Update main.tf

--- mods/main.tf
+++ mods/main.tf
@@ -10,8 +10,10 @@ module "host_project" {
   
 module "vpc" {
   source  = "./vpc"
+  project = "${module.host_project.project_id}"
   }
   
 module "subnets" {
   source  = "./subnets"
+  project = "${module.vpc.project}"
   }
